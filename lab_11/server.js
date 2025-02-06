61e0b47e9b5830d068650931340802873530d6eb
CrunchySubstances <r.park2@rgu.ac.uk>
2025-02-06 10:41:45 +0000

    e

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,11 +1,20 @@
+var express = require('express');
+var app = express();
+app.get('/', function(req, res){
+ res.send("Hello world! by express");
+});
+app.listen(8080);
+
 app.get('/getform', function(req, res){
     var name = req.query.name;
     var quest = req.query.quest;
      res.send("Hi "+name+" I am sure you will "+quest) ;
     });
+
 app.post('/postform', function(req, res){
     var name = req.body.name;
     var quest = req.body.quest;
     res.send("Hi "+name+" I am sure you will "+quest) ;
 });
+
 app.use(express.urlencoded({extended:true}))
